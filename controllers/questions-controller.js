80d8194121e87a5b231cfbb09c42d1a13f92b64a
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-04-10 13:07:27 -0400

    server set up

---
+++ controllers/questions-controller.js
@@ -0,0 +1,15 @@
+const questionService
+    = require("../services/questions/questions-service")
+
+module.exports = (app) => {
+    const findQuestionsForQuiz = (req, res) => {
+        const quizId = req.params['qid']
+        const questions
+            = questionService.findQuestionsForQuiz(quizId)
+        res.json(questions)
+    }
+
+    app.get("/api/quizzes/:qid/questions",
+        findQuestionsForQuiz)
+}
+
