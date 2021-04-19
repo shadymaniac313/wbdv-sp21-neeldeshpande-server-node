cbddc95d35a8347d7899e098104dddcb0afdc352
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-04-19 00:04:01 -0400

    installed body-parser to modules

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
